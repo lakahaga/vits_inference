38d6cb5268d137c30ecaa73fa8375f44214e1160
YERIN CHOI <lakahaga@u.sogang.ac.kr>
2022-12-14 19:15:48 +0900

    update cuda version

--- install_cuda_compatible_torch.sh
+++ install_cuda_compatible_torch.sh
@@ -1,1 +1,1 @@
-pip install torch==1.13.0 torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch==1.13.0 --extra-index-url https://download.pytorch.org/whl/cu116
